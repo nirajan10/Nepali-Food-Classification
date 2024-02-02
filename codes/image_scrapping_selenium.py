7d14109edb282f3d33e4d356fcc9752fb9e46d36
Nirajan Thakuri <thakurinirajan1999@gmail.com>
2024-02-03 01:07:43 +0545

    Small debug on code.

--- codes/image_scrapping_selenium.py
+++ codes/image_scrapping_selenium.py
@@ -6,29 +6,26 @@ import time
 import urllib
 
 food_list = {
-    'momos' : 20,
-    'dhalbhat' : 20,
-    'selroti' : 20
+    'momos' : 100,
+    'dalbhat' : 100,
+    'selroti' : 100
     }
 
 for food, n in food_list.items():
     options = webdriver.ChromeOptions()
-    options.add_argument('--user-data-dir=C:/Users/Nirajan/AppData/Local/Google/Chrome/User\ Data/Default')
+    options.add_argument('--user-data-dir=C:/Users/Nirajan/AppData/Local/Google/Chrome/User Data/Default')
     driver = uc.Chrome(options=options)
-    driver.maximize_window()
+    driver.minimize_window()
 
     url = str(f'https://www.google.com/search?sca_esv=3cbe9f3cb5d0638a&sxsrf=ACQVn08s6Q6kamb4wJ51lyk25b7qXWtKmQ:1706887592475&q={food}&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjszPvk-4yEAxUZSGwGHWIlDc8Q0pQJegQIEhAB&biw=1536&bih=730&dpr=1.25')
     driver.get(url)
     
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
     time.sleep(5)
-    if (n > 50):
-        for i in range(0, 3):
-            driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-            time.sleep(2)
-    elif (n > 20):
+    j = n//30
+    for i in range(j):
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-        time.sleep(2)
+        time.sleep(5)
 
     img_results = driver.find_elements(By.XPATH, "//img[contains(@class, 'Q4LuWd')]")
 
@@ -39,7 +36,7 @@ for food, n in food_list.items():
     file_name = food.replace('+', '_')
     folder_path = f'./dataset/{file_name}/'
 
-    for i in range(n):
+    for i in range(n):   
         urllib.request.urlretrieve(str(image_urls[i]), f"{folder_path}{i}.jpg")
 
     driver.quit()
