91e6e52947243b8948167f082459b33b43c02d84
Nirajan Thakuri <thakurinirajan1999@gmail.com>
2024-02-29 12:21:25 +0545

    Fixed error when uploading no images or other files

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from torchvision.transforms import transforms
 from torchvision import models
 from PIL import Image
 import os
+from PIL import UnidentifiedImageError
 
 app = Flask(__name__)
 
@@ -56,25 +57,31 @@ def predict_image(model, image):
 def hello_world():
     return render_template('Image Classification.html')
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
         # Check if the 'image' key is in the request.files dictionary
         if 'image' in request.files:
-            image_file = request.files['image']
-            image = Image.open(image_file)
-            image = image.convert('RGB')
-            output = predict_image(model, image)
-            pred=f"Food is {output['predicted_class']} with {output['confidence']:.2f} confidence."
-            des=f"{df.loc[output['predicted_class']][0]}"
-            n_pred=f"Food doesn't belong to any category."
-            des2="It could be the result of things like food that isn't from Nepal, food that falls into more than one category, or food that isn't even food."
+            try:
+                image_file = request.files['image']
+                image = Image.open(image_file)
+                image = image.convert('RGB')
+                output = predict_image(model, image)
+                pred = f"Food is {output['predicted_class']} with {output['confidence']:.2f} confidence."
+                des = f"{df.loc[output['predicted_class']][0]}"
+                n_pred = "Food doesn't belong to any category."
+                des2 = "It could be the result of things like food that isn't from Nepal, food that falls into more than one category, or food that isn't even food."
 
-            if output['confidence'] >= 80:
-                return render_template('Image Classification.html', pred=pred, des=des, image=image)
-            else:
-                return render_template('Image Classification.html', pred=n_pred, des2=des2, image=image)
-    
+                if output['confidence'] >= 80:
+                    return render_template('Image Classification.html', pred=pred, des=des, image=image)
+                else:
+                    return render_template('Image Classification.html', pred=n_pred, des2=des2, image=image)
+            except UnidentifiedImageError:
+                no_image_message = "Please upload a valid image."
+                return render_template('Image Classification.html', no_image=no_image_message)
+        else:
+            no_image_message = "Please insert the image"
+            return render_template('Image Classification.html', no_image=no_image_message)
     # Handle GET request or invalid POST request
     return render_template('Image Classification.html')
 
